5d0926622c82788f37c922e827f06beacc44c522
HEE-JE <windba78@naver.com>
2022-05-13 15:54:52 +0900

    inner join

--- sql-practices/sql연습/join연습.sql
+++ sql-practices/sql연습/join연습.sql
@@ -7,14 +7,36 @@ select a.first_name, b.title
 	and b.to_date = '9999-01-01';	-- row 선택 조건
     
 -- 예제2: 현재 직원의 이름과 직책을 출력하되 여성 엔지니어(engineer)만 출력하세요.
-select a.first_name, b.title
+select a.first_name, a.gender, b.title
 	from employees as a, titles as b 
-    where a.emp_no = b.emp_no
-	and b.to_date = '9999-01-01'
-    and a.gender = 'f'
-    and b.title = 'engineer';
+    where a.emp_no = b.emp_no		-- join 조건(n-1)
+	and b.to_date = '9999-01-01'	-- row 선택 조건1
+    and a.gender = 'f'				-- row 선택 조건2
+    and b.title = 'engineer';		-- row 선택 조건3
     
 -- ANSI/ISO SQL1999 JOIN 표준문법
 -- 1) natural join
+-- 두 테이블에 이름이 같은 공통 컬럼이 있으면 조인 조건을 명시하지 않아도 암묵적으로 조인이 됨.
+select a.first_name, b.title
+	from employees as a natural join titles as b -- emp_no로 join
+    where b.to_date = '9999-01-01';	-- row 선택 조건
+    
 -- 2) join ~ using
+-- natural join의 문제점
+select count(*)
+	from salaries a natural join titles b
+    where a.to_date = '9999-01-01'
+    and b.to_date = '9999-01-01';
+
+select count(*)
+	from salaries a join titles b using (emp_no)
+    where a.to_date = '9999-01-01'
+    and b.to_date = '9999-01-01';
+    
 -- 3) join ~ on
+select b.title, avg(a.salary)
+	from salaries a join titles b on a.emp_no = b.emp_no
+    where a.to_date = '9999-01-01'
+    and b.to_date = '9999-01-01'
+    group by b.title
+    order by avg(a.salary) desc;
