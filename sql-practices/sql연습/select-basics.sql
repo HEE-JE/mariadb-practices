330a7032e138e3baa4c36e2cfb8a7db36dc9dc85
HEE-JE <windba78@naver.com>
2022-05-13 11:00:58 +0900

    select basicw #2

--- sql-practices/sql연습/select-basics.sql
+++ sql-practices/sql연습/select-basics.sql
@@ -15,9 +15,27 @@ select concat(first_name, last_name) as '이름', gender as '성별', hire_date 
 select distinct(title) from titles;
 
 -- where절 #1
--- 예제:1991년 이전에 입사한 직원의 이름, 성별, 입사일을 출력
+-- 예제: 1991년 이전에 입사한 직원의 이름, 성별, 입사일을 출력
 select concat(first_name, last_name) as '이름', gender as '성별', hire_date '입사일' from employees where hire_date < '1991-01-01' order by hire_date desc;
 
 -- where절 #2: 논리 연산자
--- 예제:1989년 이전에 입사한 여직원의 이름, 성별, 입사일을 출력
+-- 예제: 1989년 이전에 입사한 여직원의 이름, 성별, 입사일을 출력
 select concat(first_name, last_name) as '이름', gender as '성별', hire_date '입사일' from employees where hire_date < '1989-01-01' and gender = 'F' order by hire_date desc;
+
+-- where절 #3: in 연산자
+-- 예제: dept_emp 테이블에서 부서번호가 d005 이거나 d009에 속한 사원의 사번, 부서번호를 출력
+select emp_no, dept_no from dept_emp where dept_no = 'd005' or dept_no = 'd009';
+select emp_no, dept_no from dept_emp where dept_no in('d005', 'd009');
+
+-- where절 #4: Like 검색
+-- 예제: 1989년에 입사한 직원의 이름, 성별, 입사일을 출력
+select concat(first_name, last_name) as '이름', gender as '성별', hire_date '입사일' from employees where '1989-01-01' <= hire_date and hire_date <= '1989-12-31';
+select concat(first_name, last_name) as '이름', gender as '성별', hire_date '입사일' from employees where hire_date like '1989%';
+select concat(first_name, last_name) as '이름', gender as '성별', hire_date '입사일' from employees where hire_date between '1989-01-01' and '1989-12-31';
+
+-- order by절
+-- 예제1: 남자 직원의 이름, 성별, 입사일을 입사일순(선임순)으로 출력
+select concat(first_name, last_name) as '이름', gender as '성별', hire_date '입사일' from employees where gender = 'm' order by hire_date asc;
+
+-- 예제2: 직원의 사번, 월급을 사번과 월급순으로 출력
+select emp_no, salary, from_date, to_date from salaries order by emp_no asc, salary desc;
